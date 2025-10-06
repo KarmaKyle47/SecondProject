d80b1c0d8b4e22e241523499ad7ef3280539775d
KarmaKyle47 <kylepeterson02@gmail.com>
2025-10-06 16:19:03 -0400

    Sample patch connected on bottom edge.

--- SampleTrajectoryPatches.R
+++ SampleTrajectoryPatches.R
@@ -1,10 +1,5 @@
-
 library(MASS)
 library(plotly)
-
-border = c(0,0,2,3)
-
-k=0.5
 
 samplePatch_Unrestricted = function(border, k){
 
@@ -39,16 +34,57 @@ samplePatch_Unrestricted = function(border, k){
   coef
 
 }
+
 samplePatch_Known_Bottom = function(border, k, bottom_coefs, bottom_border){
 
   dx = border[3] - border[1]
   dy = border[4] - border[2]
 
+  A = matrix(c(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+               0,0,dx,0,dx^2,0,dx^3,0,0,0,0,0,0,0,0,0,
+               0,dy,0,0,0,0,0,0,dy^2,dy^3,0,0,0,0,0,0,
+               0,0,0,dx*dy,0,dx^2*dy,0,dx^3*dy,0,0,dx*dy^2,dx*dy^3,dx^2*dy^2,dx^2*dy^3,dx^3*dy^2,dx^3*dy^3,
+               0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,
+               0,0,1,0,2*dx,0,3*dx^2,0,0,0,0,0,0,0,0,0,
+               0,0,1,dy,0,0,0,0,0,0,dy^2,dy^3,0,0,0,0,
+               0,0,1,dy,2*dx,2*dx*dy,3*dx^2,3*dx^2*dy,0,0,dy^2,dy^3,2*dx*dy^2,2*dx*dy^3,3*dx^2*dy^2,3*dx^2*dy^3,
+               0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+               0,1,0,dx,0,dx^2,0,dx^3,0,0,0,0,0,0,0,0,
+               0,1,0,0,0,0,0,0,2*dy,3*dy^2,0,0,0,0,0,0,
+               0,1,0,dx,0,dx^2,0,dx^3,2*dy,3*dy^2,2*dx*dy,3*dx*dy^2,2*dx^2*dy,3*dx^2*dy^2,2*dx^3*dy,3*dx^3*dy^2,
+               0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,
+               0,0,0,1,0,2*dx,0,3*dx^2,0,0,0,0,0,0,0,0,
+               0,0,0,1,0,0,0,0,0,0,2*dy,3*dy^2,0,0,0,0,
+               0,0,0,1,0,2*dx,0,3*dx^2,0,0,2*dy,3*dy^2,4*dx*dy,6*dx*dy^2,6*dx^2*dy,9*dx^2*dy^2), nrow = 16, ncol = 16, byrow = T)
+
+  c00 = evaluateCubicPatchValue(coef = bottom_coefs, border = bottom_border, curPos = c(border[1], border[2]))
+  c01 = evaluateCubicPatchParY(coef = bottom_coefs, border = bottom_border, curPos = c(border[1], border[2]))
+  c10 = evaluateCubicPatchParX(coef = bottom_coefs, border = bottom_border, curPos = c(border[1], border[2]))
+  c11 = evaluateCubicPatchParXY(coef = bottom_coefs, border = bottom_border, curPos = c(border[1], border[2]))
+
+  value_BR = evaluateCubicPatchValue(coef = bottom_coefs, border = bottom_border, curPos = c(border[3], border[2]))
+  par_X_BR = evaluateCubicPatchParX(coef = bottom_coefs, border = bottom_border, curPos = c(border[3], border[2]))
+  par_Y_BR = evaluateCubicPatchParY(coef = bottom_coefs, border = bottom_border, curPos = c(border[3], border[2]))
+  par_XY_BR = evaluateCubicPatchParXY(coef = bottom_coefs, border = bottom_border, curPos = c(border[3], border[2]))
+
+  c20 = (-1*par_X_BR*dx + 3*value_BR - 3*c00 - 2*c10*dx)/(dx^2)
+  c21 = (-1*par_XY_BR*dx + 3*par_Y_BR - 3*c01 - 2*c11*dx)/(dx^2)
+  c30 = (par_X_BR*dx - 2*value_BR + 2*c00 + c10*dx)/(dx^3)
+  c31 = (par_XY_BR*dx - 2*par_Y_BR + 2*c01 + c11*dx)/(dx^3)
 
 
+  known_coefs = c(c00, c01, c10, c11, c20, c21, c30, c31)
 
+  mu_trans = -1*(A[c(3,4,7,8,11,12,15,16), 1:8] %*% known_coefs)
 
+  A_inv = solve(A[c(3,4,7,8,11,12,15,16), 9:16])
 
+  mu = A_inv %*% mu_trans
+  sigma = k^2*(A_inv %*% t(A_inv))
+
+  coef = mvrnorm(mu = mu, Sigma = sigma)
+
+  c(known_coefs, coef)
 
 }
 
@@ -60,7 +96,7 @@ evaluateCubicPatchValue = function(coef, border, curPos){
   x = curPos[1]
   y = curPos[2]
 
-  polyTerms = c(1, (y-y_0), (x-x_0), (x-x_0)^2,
+  polyTerms = c(1, (y-y_0), (x-x_0), (x-x_0)*(y-y_0),
                 (x-x_0)^2, (x-x_0)^2*(y-y_0), (x-x_0)^3, (x-x_0)^3*(y-y_0),
                 (y-y_0)^2, (y-y_0)^3, (x-x_0)*(y-y_0)^2, (x-x_0)*(y-y_0)^3,
                 (x-x_0)^2*(y-y_0)^2, (x-x_0)^2*(y-y_0)^3, (x-x_0)^3*(y-y_0)^2, (x-x_0)^3*(y-y_0)^3)
@@ -75,7 +111,7 @@ evaluateCubicPatchParX = function(coef, border, curPos){
   x = curPos[1]
   y = curPos[2]
 
-  polyTerms = c(0, 0, 1, 2*(x-x_0),
+  polyTerms = c(0, 0, 1, (y-y_0),
                 2*(x-x_0), 2*(x-x_0)*(y-y_0), 3*(x-x_0)^2, 3*(x-x_0)^2*(y-y_0),
                 0, 0, (y-y_0)^2, (y-y_0)^3,
                 2*(x-x_0)*(y-y_0)^2, 2*(x-x_0)*(y-y_0)^3, 3*(x-x_0)^2*(y-y_0)^2, 3*(x-x_0)^2*(y-y_0)^3)
@@ -89,7 +125,7 @@ evaluateCubicPatchParY = function(coef, border, curPos){
   x = curPos[1]
   y = curPos[2]
 
-  polyTerms = c(0, 1, 0, 0,
+  polyTerms = c(0, 1, 0, (x-x_0),
                 0, (x-x_0)^2, 0, (x-x_0)^3,
                 2*(y-y_0), 3*(y-y_0)^2, 2*(x-x_0)*(y-y_0), 3*(x-x_0)*(y-y_0)^2,
                 2*(x-x_0)^2*(y-y_0), 3*(x-x_0)^2*(y-y_0)^2, 2*(x-x_0)^3*(y-y_0), 3*(x-x_0)^3*(y-y_0)^2)
@@ -105,7 +141,7 @@ evaluateCubicPatchParXY = function(coef, border, curPos){
   x = curPos[1]
   y = curPos[2]
 
-  polyTerms = c(0, 0, 0, 0,
+  polyTerms = c(0, 0, 0, 1,
                 0, 2*(x-x_0), 0, 3*(x-x_0)^2,
                 0, 0, 2*(y-y_0), 3*(y-y_0)^2,
                 4*(x-x_0)*(y-y_0), 6*(x-x_0)*(y-y_0)^2, 6*(x-x_0)^2*(y-y_0), 9*(x-x_0)^2*(y-y_0)^2)
@@ -113,11 +149,13 @@ evaluateCubicPatchParXY = function(coef, border, curPos){
   sum(coef * polyTerms)
 }
 
+border_init = c(0,0,2,3)
 
+init_coef = samplePatch_Unrestricted(border_init, 0.5)
 
 test_grid = expand.grid(seq(0,2, 0.1), seq(0,3,0.1))
 
-PatchValues = apply(test_grid, 1, FUN = evaluateCubicPatchValue, coef = coef, border = border)
+PatchValues = apply(test_grid, 1, FUN = evaluateCubicPatchValue, coef = init_coef, border = border_init)
 
 plottingGrid = data.frame(test_grid, PatchValues)
 names(plottingGrid) = c('X','Y','Value')
@@ -149,13 +187,63 @@ plotCubicPatch3D = function(coef, border, grid_size){
       yaxis = list(title = 'Y'),
       zaxis = list(
         title = 'Value',
-        range = c(-200, 200) # <-- This sets the Z-axis limit
+        range = c(-5, 6) # <-- This sets the Z-axis limit
       )
     )
   )
 
 }
 
-rand_coef = samplePatch_Unrestricted(border, 10)
-plotCubicPatch3D(rand_coef, border, 0.1)
+border_init = c(0,0,2,3)
+
+border_connect = c(0.5,3,1.5,4)
+
+init_coef = samplePatch_Unrestricted(border_init, 0.5)
+connect_coef = samplePatch_Known_Bottom(border = border_connect, k = 0.5, bottom_coefs = init_coef, bottom_border = border_init)
+
+
+
+
+plotCubicPatch3D(init_coef, border_init, 0.1)
+
+
+
+test_grid_init = expand.grid(seq(0,2, 0.1), seq(0,3,0.1))
+PatchValues = apply(test_grid_init, 1, FUN = evaluateCubicPatchValue, coef = init_coef, border = border_init)
+
+plottingGrid = data.frame(test_grid, PatchValues)
+names(plottingGrid) = c('X','Y','Value')
+
+test_grid_connect = expand.grid(seq(0.5,1.5, 0.1), seq(3,4,0.1))
+PatchValues_Connect = apply(test_grid_connect, 1, FUN = evaluateCubicPatchValue, coef = connect_coef, border = border_connect)
+
+PlottingGrid_Connect = data.frame(test_grid_connect, PatchValues_Connect)
+names(PlottingGrid_Connect) = c('X','Y','Value')
+
+PlottingGrid_Combined = rbind(plottingGrid, PlottingGrid_Connect)
+
+Value <- xtabs(Value ~ X + Y, data = PlottingGrid_Combined)
+X <- as.numeric(rownames(Value))
+Y <- as.numeric(colnames(Value))
+
+plot = plot_ly(x = ~X, y = ~Y, z = ~Value, type = "surface")
+
+plot %>% layout(
+  title = "3D Surface with Z-Axis Limit",
+  scene = list(
+    xaxis = list(title = 'X'),
+    yaxis = list(title = 'Y'),
+    zaxis = list(
+      title = 'Value',
+      range = c(-5, 6) # <-- This sets the Z-axis limit
+    )
+  )
+)
+
+plot_ly(PlottingGrid_Combined, x = ~X, y = ~Y, z = ~Value, type = "scatter3d", mode = "markers")
+
+
+
+
+
 
